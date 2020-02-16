7920e1c62d31f546d5ddc87127641bb194974137
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-16 16:56:59 -0300

    ref: Push down methods in SuperclassExtracted

--- moneta-convert/moneta-convert-base/src/main/java/org/javamoney/moneta/convert/internal/SuperclassExtracted.java
+++ moneta-convert/moneta-convert-base/src/main/java/org/javamoney/moneta/convert/internal/SuperclassExtracted.java
@@ -1,16 +1,10 @@
 package org.javamoney.moneta.convert.internal;
 
-import java.math.BigDecimal;
-
-import javax.money.convert.ConversionQuery;
-import javax.money.convert.ExchangeRate;
 import javax.money.convert.ProviderContext;
 import javax.money.convert.ProviderContextBuilder;
 import javax.money.convert.RateType;
 
-import org.javamoney.moneta.convert.ExchangeRateBuilder;
 import org.javamoney.moneta.spi.AbstractRateProvider;
-import org.javamoney.moneta.spi.DefaultNumberValue;
 
 public abstract class SuperclassExtracted extends AbstractRateProvider {
 
@@ -21,39 +15,6 @@ public abstract class SuperclassExtracted extends AbstractRateProvider {
 
 	public SuperclassExtracted(ProviderContext providerContext) {
 		super(providerContext);
-	}
-
-	/**
-	 * Check if this provider can provide a rate, which is only the case if base and term are equal.
-	 *
-	 * @param conversionQuery the required {@link ConversionQuery}, not {@code null}
-	 * @return true, if the contained base and term currencies are known to this provider.
-	 */
-	@Override
-	public boolean isAvailable(ConversionQuery conversionQuery) {
-	    return conversionQuery.getBaseCurrency().getCurrencyCode()
-	            .equals(conversionQuery.getCurrency().getCurrencyCode());
-	}
-
-	@Override
-	public ExchangeRate getExchangeRate(ConversionQuery conversionQuery) {
-	    if (conversionQuery.getBaseCurrency().getCurrencyCode().equals(conversionQuery.getCurrency().getCurrencyCode())) {
-	        ExchangeRateBuilder builder = new ExchangeRateBuilder(getContext().getProviderName(), RateType.OTHER)
-	                .setBase(conversionQuery.getBaseCurrency());
-	        builder.setTerm(conversionQuery.getCurrency());
-	        builder.setFactor(DefaultNumberValue.of(BigDecimal.ONE));
-	        return builder.build();
-	    }
-	    return null;
-	}
-
-	@Override
-	public ExchangeRate getReversed(ExchangeRate rate) {
-	    if (rate.getContext().getProviderName().equals(CONTEXT.getProviderName())) {
-	        return new ExchangeRateBuilder(rate.getContext()).setTerm(rate.getBaseCurrency())
-	                .setBase(rate.getCurrency()).setFactor(new DefaultNumberValue(BigDecimal.ONE)).build();
-	    }
-	    return null;
 	}
 
 }
